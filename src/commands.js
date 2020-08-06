b0f930144b4300c00a447ac25304067e7e838dcd
kovuko <jason.eh20@gmail.com>
2020-08-06 05:25:12 -0700

    Module support complete

--- src/commands.js
+++ src/commands.js
@@ -1,10 +1,9 @@
-import { enjo } from './enjo';
-
+const {enjo} = require('./enjo');
 let prefix = require('../json/config.json').prefix;
 
 enjo.on('message', message => {
     if(message.content.startsWith(prefix) && !message.author.bot) {
-        let cmd = message.content.slice(prefix).split(' ');
+        let cmd = message.content.slice(prefix.length).split(' ');
         switch(cmd[0]) {
             case 'uptime':
                 message.channel.send(`I've been alive for ${enjo.uptime}`);
--- src/enjo.js
+++ src/enjo.js
@@ -1,12 +1,23 @@
 const Discord = require('discord.js');
 const colors = require('colors');
+const fs = require('fs');
+
 const token = require('../json/token.json');
 const enjo = new Discord.Client();
+module.exports = {enjo};
+
+const sources = fs.readdirSync('./src');
 
 enjo.on('ready', () => {
     console.log('Bot Online!'.cyan);
 });
 
-enjo.login(token);
+sources.forEach(async file => {
+    if(file.endsWith('.js') && !file.startsWith('enjo')) {
+        await require(`./${file}`);
+        console.log(`${file} loaded`.yellow);
+    }
+});
 
-module.exports = {enjo};
+
+enjo.login(token);
