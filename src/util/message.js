2b9e1d11e16fc31d47270d8da0300d76acfed2c9
kovuko <jason.eh20@gmail.com>
2020-08-06 06:11:32 -0700

    command prep

--- src/util/message.js
+++ src/util/message.js
@@ -1,6 +1,7 @@
 const {enjo} = require('../enjo');
+const prefix = require('../json/config.json').prefix;
 
 enjo.on('message', message => {
     if(message.author.bot) return;
-    
+    if(message.content.startsWith(prefix)) require('../modules/commands');
 });
