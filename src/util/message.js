14c505750e7bf19962c86074f3fe8b2cd1f4aa30
kovuko <jason.eh20@gmail.com>
2020-08-14 14:29:10 -0700

    added new module and changed command behavior

--- src/util/message.js
+++ src/util/message.js
@@ -3,8 +3,9 @@ const {enjo} = require('../enjo');
 const prefix = require('../json/config.json').prefix;
 
 enjo.on('message', message => {
-    if(message.author.bot) return;
     if(message.content.startsWith(prefix)) {
+        if(message.author.bot) return;
+        
         require('../modules/commands').CommandHandler.entry(message);
     }
 });
